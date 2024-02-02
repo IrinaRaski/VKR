ccef00b617bb7042bde387878e1761ce09dd69b7
irinaraski <irina.raski@mail.ru>
2024-02-02 12:08:33 +0300

    background

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ import numpy as np
 from flask import Flask, render_template, request
 import pickle
 
-app_3 = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 path = r'model/model.pkl'
-url = r'D:\PROJECTS\VKR\html\background.jpg'
-model = pickle.load(open(path, 'rb'))
+model = pickle.load(open(r'model/model.pkl', 'rb'))
 
 
-@app_3.route('/', methods=['get', 'post'])
+@app.route('/', methods=['get', 'post'])
 def main():
     if request.method == 'GET':
         return render_template('main.html')
