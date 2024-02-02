eb11cac7562d906a6ea17bd365955c12f1116725
irinaraski <irina.raski@mail.ru>
2024-02-02 11:02:07 +0300

    Flask

---
+++ app.py
@@ -0,0 +1,21 @@
+import numpy as np
+from flask import Flask, render_template, request
+import pickle
+
+app_3 = Flask(__name__, template_folder='templates')
+path = r'C:\Users\Ирина\PycharmProjects\VKR\model\model.pkl'
+url = r'C:\Users\Ирина\Desktop\DATA SCIENCE\ВКР\HTML\background.jpg'
+model = pickle.load(open(path, 'rb'))
+
+
+@app_3.route('/', methods=['get', 'post'])
+def main():
+    if request.method == 'GET':
+        return render_template('main.html')
+
+    if request.method == 'POST':
+        prediction_text = [float(x) for x in request.form.values()]
+        x_new = [np.array(prediction_text)]
+        prediction = model.predict(x_new).flatten()
+        result = f'{str(prediction)[1:-1]}'
+        return render_template('main.html', result=result)
